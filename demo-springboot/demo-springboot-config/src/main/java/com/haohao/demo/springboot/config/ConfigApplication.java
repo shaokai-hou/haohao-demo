8ace37ff085e0319e12c9e77f1bab64f36245569
haohao <haohao_hsk@163.com>
2023-04-06 12:11:38 +0800

    springboot使用定时任务

--- demo-springboot/demo-springboot-config/src/main/java/com/haohao/demo/springboot/config/ConfigApplication.java
+++ demo-springboot/demo-springboot-config/src/main/java/com/haohao/demo/springboot/config/ConfigApplication.java
@@ -1,12 +1,11 @@
-package com.haohao.demo.springboot;
-
+package com.haohao.demo.springboot.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class ConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
---
+++ demo-springboot/demo-springboot-timer/src/main/java/com/haohao/demo/springboot/timer/DemoTimer.java
@@ -0,0 +1,23 @@
+package com.haohao.demo.springboot.timer;
+
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DemoTimer {
+
+    @Scheduled(cron = "00 10 00 * * ?")
+    public void test() {
+        // 每天凌晨00点10分执行一次
+        log.info("我被执行了 test:{}", DateUtil.now());
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void interval() {
+        // 一秒钟执行一次
+        log.info("我被执行了 interval:{}", DateUtil.now());
+    }
+}
