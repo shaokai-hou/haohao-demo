efb33d8516769e8f60f6642e142fa7fcf3a1b9be
haohao <haohao_hsk@163.com>
2023-04-12 18:06:32 +0800

    README.md

--- demo-date/src/main/java/com/haohao/demo/date/Countdown.java
+++ demo-date/src/main/java/com/haohao/demo/date/Countdown.java
@@ -12,14 +12,6 @@ public class Countdown {
 
     public static LocalDateTime nowDateTime = LocalDateTime.now();
 
-    public static void main(String[] args) {
-        System.out.println("【摸鱼办】提醒您：下午好，摸鱼人！当前时间为：" + nowDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        System.out.println("工作再累，一定不要忘记摸鱼哦！有事没事起身去茶水间、去厕所、去廊道走走，别老在工位上坐着。钱是老板的，命是自己的！");
-        System.out.println(Countdown.getResult("距今日下班", LocalDateTime.of(nowDateTime.getYear(), nowDateTime.getMonth(), nowDateTime.getDayOfMonth(), 18, 0)));
-        System.out.println(Countdown.getResult("距周五下班", LocalDateTime.of(2023, 4, 14, 0, 0)));
-        System.out.println(Countdown.getResult("距下次发薪", LocalDateTime.of(2023, 4, 30, 0, 0)));
-    }
-
     public static String getResult(String str, LocalDateTime dateTime) {
         return StrUtil.format("{} {} 时问还有：{}", str, Countdown.getDate(dateTime), Countdown.getDateDescribe(dateTime));
     }
@@ -30,5 +22,18 @@ public class Countdown {
 
     public static String getDateDescribe(LocalDateTime dateTime) {
         return DateUtil.formatBetween(Duration.between(Countdown.nowDateTime, dateTime).toMillis(), BetweenFormatter.Level.SECOND);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("【摸鱼办】提醒您：下午好，摸鱼人！当前时间为：" + nowDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        System.out.println("工作再累，一定不要忘记摸鱼哦！有事没事起身去茶水间、去厕所、去廊道走走，别老在工位上坐着。钱是老板的，命是自己的！");
+        System.out.println(Countdown.getResult("距今日下班", LocalDateTime.of(nowDateTime.getYear(), nowDateTime.getMonth(), nowDateTime.getDayOfMonth(), 18, 0)));
+        System.out.println(Countdown.getResult("距周五下班", LocalDateTime.of(2023, 4, 14, 0, 0)));
+        System.out.println(Countdown.getResult("距下次发薪", LocalDateTime.of(2023, 4, 30, 0, 0)));
+        System.out.println(Countdown.getResult("距离劳动节", LocalDateTime.of(2023, 5, 1, 0, 0)));
+        System.out.println(Countdown.getResult("距离端午节", LocalDateTime.of(2023, 6, 22, 0, 0)));
+        System.out.println(Countdown.getResult("距离中秋节", LocalDateTime.of(2023, 9, 29, 0, 0)));
+        System.out.println(Countdown.getResult("距离国庆节", LocalDateTime.of(2023, 10, 1, 0, 0)));
+        System.out.println(Countdown.getResult("距离元旦节", LocalDateTime.of(2024, 1, 1, 0, 0)));
     }
 }
