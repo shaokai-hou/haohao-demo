93152625ff565f7ac637a38b2ab0fa8413750363
haohao <haohao_hsk@163.com>
2023-04-13 14:33:56 +0800

    README.md

--- demo-date/src/main/java/com/haohao/demo/date/Countdown.java
+++ demo-date/src/main/java/com/haohao/demo/date/Countdown.java
@@ -3,11 +3,19 @@ package com.haohao.demo.date;
 import cn.hutool.core.date.BetweenFormatter;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
+/**
+ * 假期倒计时
+ */
 public class Countdown {
 
     public static LocalDateTime nowDateTime = LocalDateTime.now();
@@ -24,16 +32,29 @@ public class Countdown {
         return DateUtil.formatBetween(Duration.between(Countdown.nowDateTime, dateTime).toMillis(), BetweenFormatter.Level.SECOND);
     }
 
+    public static List<JSONObject> getHolidays() {
+        String result = HttpUtil.get("http://timor.tech/api/holiday/year/2023/");
+        JSONObject obj = JSONUtil.parseObj(result);
+        if (StrUtil.equals("0", obj.getStr("code"))) {
+
+            JSONObject holiday = obj.getJSONObject("holiday");
+            // 过滤节假日、排序
+            return holiday.keySet()
+                    .stream()
+                    .map(holiday::getJSONObject)
+                    .filter(item -> item.getBool("holiday") && Countdown.nowDateTime.isBefore(item.getLocalDateTime("date", LocalDateTime.now())))
+                    .collect(Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(item -> item.getStr("name")))), LinkedList::new))
+                    .stream().sorted(Comparator.comparing(o -> o.getLocalDateTime("date", LocalDateTime.now()).toLocalDate())).collect(Collectors.toList());
+        }
+        return new ArrayList<>();
+    }
+
     public static void main(String[] args) {
+
+        List<JSONObject> list = getHolidays();
         System.out.println("【摸鱼办】提醒您：下午好，摸鱼人！当前时间为：" + nowDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         System.out.println("工作再累，一定不要忘记摸鱼哦！有事没事起身去茶水间、去厕所、去廊道走走，别老在工位上坐着。钱是老板的，命是自己的！");
         System.out.println(Countdown.getResult("距今日下班", LocalDateTime.of(nowDateTime.getYear(), nowDateTime.getMonth(), nowDateTime.getDayOfMonth(), 18, 0)));
-        System.out.println(Countdown.getResult("距周五下班", LocalDateTime.of(2023, 4, 14, 0, 0)));
-        System.out.println(Countdown.getResult("距下次发薪", LocalDateTime.of(2023, 4, 30, 0, 0)));
-        System.out.println(Countdown.getResult("距离劳动节", LocalDateTime.of(2023, 5, 1, 0, 0)));
-        System.out.println(Countdown.getResult("距离端午节", LocalDateTime.of(2023, 6, 22, 0, 0)));
-        System.out.println(Countdown.getResult("距离中秋节", LocalDateTime.of(2023, 9, 29, 0, 0)));
-        System.out.println(Countdown.getResult("距离国庆节", LocalDateTime.of(2023, 10, 1, 0, 0)));
-        System.out.println(Countdown.getResult("距离元旦节", LocalDateTime.of(2024, 1, 1, 0, 0)));
+        list.forEach(item -> System.out.println(Countdown.getResult("距离" + item.getStr("name"), item.getLocalDateTime("date", LocalDateTime.now()))));
     }
 }
