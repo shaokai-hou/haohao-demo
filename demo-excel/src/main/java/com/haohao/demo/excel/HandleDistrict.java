926b225bedfa74ce8b61a091e07e2b2de5a4d8ba
haohao <haohao_hsk@163.com>
2023-04-19 13:39:26 +0800

    配置文件

--- demo-excel/src/main/java/com/haohao/demo/excel/HandleDistrict.java
+++ demo-excel/src/main/java/com/haohao/demo/excel/HandleDistrict.java
@@ -1,11 +1,17 @@
 package com.haohao.demo.excel;
 
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.extra.spring.SpringUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.listener.ReadListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,21 +22,51 @@ public class HandleDistrict {
 
     public List<Map<Integer, String>> handleData() {
         List<Map<Integer, String>> data = new ArrayList<>();
+        Integer batchSize = 100;
 
-        String fileName = "/Users/haohao/Documents/weather_district_id.xlsx";
+        String fileName = "/Users/haohao/Documents/数据/weather_district_id.xlsx";
         EasyExcel.read(fileName, new ReadListener<Map<Integer, String>>() {
 
             @Override
             public void invoke(Map<Integer, String> excelData, AnalysisContext analysisContext) {
                 log.info("解析到一条数据：{}", excelData);
+                if (data.size() == batchSize) {
+                    handleInsert(data);
+                    data.clear();
+                }
                 data.add(excelData);
             }
 
             @Override
             public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+                handleInsert(data);
                 log.info("数据解析完成！");
             }
         }).sheet().doRead();
         return data;
+    }
+
+    public void handleInsert(List<Map<Integer, String>> data) {
+        JdbcTemplate jdbcTemplate = SpringUtil.getBean(JdbcTemplate.class);
+
+        String sql = "insert into `demo_district` (`province`, `city`, `city_code`, `district`, `district_code`, `longitude`, `latitude`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                Map<Integer, String> rowMap = data.get(i);
+                ps.setString(1, MapUtil.getStr(rowMap, 1));
+                ps.setString(2, MapUtil.getStr(rowMap, 2));
+                ps.setString(3, MapUtil.getStr(rowMap, 3));
+                ps.setString(4, MapUtil.getStr(rowMap, 4));
+                ps.setString(5, MapUtil.getStr(rowMap, 5));
+                ps.setString(6, MapUtil.getStr(rowMap, 6));
+                ps.setString(7, MapUtil.getStr(rowMap, 7));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return data.size();
+            }
+        });
     }
 }
--- demo-excel/src/test/java/com/haohao/demo/excel/HandleDistrictTest.java
+++ demo-excel/src/test/java/com/haohao/demo/excel/HandleDistrictTest.java
@@ -1,15 +1,11 @@
 package com.haohao.demo.excel;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.StrUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
 
 
 @SpringBootTest
@@ -25,12 +21,12 @@ class HandleDistrictTest {
         // 过滤省份
 //        List<Map<Integer, String>> collect = mapList.stream().filter(map -> StrUtil.equals(MapUtil.getStr(map, 1), "陕西省")).collect(Collectors.toList());
         // 编码组成数组
-        StringJoiner content = new StringJoiner(",", "String[] codes = new String[]{", "};");
-        mapList.stream().filter(map -> StrUtil.equals(MapUtil.getStr(map, 1), "陕西省"))
-                .map(map -> "\"" + MapUtil.getStr(map, 5) + "\"")
-                .forEach(content::add);
-        String fileName = "/Users/haohao/Documents/weather_district_id.java";
-        FileUtil.writeUtf8String(content.toString(), fileName);
+//        StringJoiner content = new StringJoiner(",", "String[] codes = new String[]{", "};");
+//        mapList.stream().filter(map -> StrUtil.equals(MapUtil.getStr(map, 1), "陕西省"))
+//                .map(map -> "\"" + MapUtil.getStr(map, 5) + "\"")
+//                .forEach(content::add);
+//        String fileName = "/Users/haohao/Documents/weather_district_id.java";
+//        FileUtil.writeUtf8String(content.toString(), fileName);
 
         // 过滤市
 //        mapList.stream().filter(map -> StrUtil.equals(MapUtil.getStr(map, 2), "宝鸡市")).collect(Collectors.toList()).forEach(System.err::println);
